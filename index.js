2cc449c10e14f8b1de8dc3e3506dfc238adea217
josephquested <josephquested@gmail.com>
2018-12-02 23:28:18 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+name = "JO$EPH"                // string
+age = 25                       // int
+cool = true                    // bool
+children = ["addie", "dexter"] // array
+
+joseph = {
+  name: name,
+  age: age,
+  cool: cool,
+  children: children
+}
+
+console.log("welcome to sprint 3!")
+
+console.log(name)
+console.log(joseph.name)
+
+function printAddition(number1, number2) {
+    console.log(number1 + number2)
+}
+
+function add(number1, number2) {
+  return number1 + number2
+}
+
+printAddition(2, 3)
+
+console.log(add(5, 5))
